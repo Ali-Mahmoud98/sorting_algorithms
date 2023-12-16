5340a304b6f0ac3f9ecdcd7277bdc4c7e229ef0c
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-12-16 19:13:53 +0200

    .

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,24 @@
 #include "sort.h"
-
+/**
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
  * in ascending order using the Insertion sort algorithm.
@@ -8,31 +27,18 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *temp;
+	listint_t *current, *temp, *itr;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	current = (*list)->next;
-
-	for (; current != NULL; current = current->next)
+	for (itr = (*list)->next; itr != NULL; itr = itr->next)
 	{
-		temp = current;
-		for (; temp->prev != NULL && temp->n < temp->prev->n;
+		temp = itr->prev;
+		for (; temp != NULL && temp->n > itr->n;
 			temp = temp->prev)
 		{
-			/* Swap nodes if the current node's */
-			/* value is less than the previous node's value */
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-			temp->prev->next = temp->next;
-			temp->next = temp->prev;
-			temp->prev = temp->prev->prev;
-			temp->next->prev = temp;
-
-			/* Update the head of the list if needed */
-			if (temp->prev == NULL)
-				*list = temp;
+			swap_nodes(&list, &temp, itr);
 			print_list((const listint_t *)*list);
 		}
 	}
