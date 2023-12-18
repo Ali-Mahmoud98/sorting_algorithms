4aaf87a0a40e0b5a6b94b3bb7fa4ac44f7ebec9c
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-12-18 13:21:06 +0200

    .

--- sort.h
+++ sort.h
@@ -24,6 +24,7 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 /********************** End ARRAY Methods **********************/
 /****************************************************************/
 
