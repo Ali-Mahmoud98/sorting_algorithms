936a8f3285c8091c5741e118756aa96145180e88
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-12-19 23:25:15 +0200

    .

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -4,22 +4,22 @@
  * merge - Merge two subarrays into a sorted array.
  *
  * @array: The original array containing two sorted subarrays.
- * @size: The total size of the array.
+ * @buff: array to store values.
  * @left: The starting index of the left subarray.
  * @middle: The ending index of the left subarray.
  * @right: The ending index of the right subarray.
  */
 void merge(int *array, int *buff, size_t left, size_t middle, size_t right)
 {
-    size_t i, j, k = 0;
+	size_t i, j, k = 0;
 
-    printf("Merging...\n[left]: ");
+	printf("Merging...\n[left]: ");
 	print_array(&array[left], middle - left);
 
-    printf("[right]: ");
+	printf("[right]: ");
 	print_array(&array[middle], right - middle);
 
-    for (i = left, j = middle; i < middle && j < right; k++)
+	for (i = left, j = middle; i < middle && j < right; k++)
 		buff[k] = (array[i] < array[j]) ? array[i++] : array[j++];
 	for (; i < middle; i++)
 		buff[k++] = array[i];
@@ -35,25 +35,25 @@ void merge(int *array, int *buff, size_t left, size_t middle, size_t right)
  * merge_sort_recursive - Recursively apply merge sort to the array.
  *
  * @array: The array to be sorted.
- * @size: The size of the array.
+ * @buff: array to store values.
  * @left: The starting index of the subarray to be sorted.
  * @right: The ending index of the subarray to be sorted.
  */
 void merge_sort_recursive(int *array, int *buff, size_t left, size_t right)
 {
-    size_t middle;
+	size_t middle;
 
-    if (right - left > 1)
-    {
-        middle = left + (right - left) / 2;
+	if (right - left > 1)
+	{
+		middle = left + (right - left) / 2;
 
-        /* Recursively sort the first and second halves */
-        merge_sort_recursive(array, buff, left, middle);
-        merge_sort_recursive(array, buff, middle, right);
+		/* Recursively sort the first and second halves */
+		merge_sort_recursive(array, buff, left, middle);
+		merge_sort_recursive(array, buff, middle, right);
 
-        /* Merge the sorted halves */
-        merge(array, buff, left, middle, right);
-    }
+		/* Merge the sorted halves */
+		merge(array, buff, left, middle, right);
+	}
 }
 /**
  * merge_sort - function that sorts an array of integers in ascending
@@ -63,14 +63,14 @@ void merge_sort_recursive(int *array, int *buff, size_t left, size_t right)
 */
 void merge_sort(int *array, size_t size)
 {
-    int *buff;
+	int *buff;
 
-    if (array == NULL || size < 2)
-        return;
-    
-    buff = malloc(sizeof(int) * size);
-    if (!buff)
-        return;
-    merge_sort_recursive(array, buff, 0, size);
-    free(buff);
+	if (array == NULL || size < 2)
+		return;
+
+	buff = malloc(sizeof(int) * size);
+	if (!buff)
+		return;
+	merge_sort_recursive(array, buff, 0, size);
+	free(buff);
 }
